3e2bb9995f8e64e0cbae207031d53a9b081daf2f
Nikita Sechenov <nikitasechenov@192.168.1.4>
2023-06-14 07:05:23 +0300

    init_comit

---
+++ LESSON2/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int[new Random().Next(3,10)];
+void fillarray(int[] arr)
+{
+    int ind = 0;
+    while(ind<arr.Length)
+    {
+        arr[ind] = new Random().Next(0,100);
+        ind++;
+    }
+}
+void displayArr(int[] arr)
+{
+    for(int i = 0; i<arr.Length; i++)
+    Console.Write($"{arr[i]} ");
+}
+int arrMax(int[] arr)
+{
+    int max = arr[0];
+    for(int i=1; i<arr.Length; i++)
+    {
+        if(arr[i]>max) max = arr[i];
+    }
+    return max;
+}
+fillarray(array);
+displayArr(array);
+Console.WriteLine($"Largest Element of Array is {arrMax(array)}");
---
+++ seminar1/TASKS/task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input Your Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (Math.Abs(num) < 100 || Math.Abs(num) > 999)
+{
+    Console.Write("Input Your Number Again: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(Math.Abs(num%10));
