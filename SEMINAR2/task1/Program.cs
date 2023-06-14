d78c805114a0fdff553f4785a417f0ac29b7946d
Nikita Sechenov <nikitasechenov@192.168.1.4>
2023-06-14 22:24:40 +0300

    sem2_task1_1

---
+++ SEMINAR2/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Random rand = new Random(); Объявляем новый экземпляр класса Random под наименованием
+// rand, но не инициализируем.
+Random RND = new Random();
+int num = RND.Next(0,100);
+Console.WriteLine(num);
+int a = num/10;
+int b = num%10;
+if(a>b) Console.WriteLine($"Number {a} is bigger than {b}");
+else if(b>a) Console.WriteLine($"Number {b} is bigger than {a}");
+else Console.WriteLine($"Number {b} is equal to {a}");
