bff9489188dfa149622b84fae739527104f4d050
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-15 19:35:09 +0300

    sem2task3

---
+++ SEMINAR2/task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Random RND = new Random();
+int num = RND.Next(1,1000);
+//num=322;
+if(num%7==0 && num%23==0)
+Console.WriteLine($"The number {num} is divided by 7 and 23 without remainder.");
+else
+Console.WriteLine($"The Number {num} is not divided by 7 or 23.");
