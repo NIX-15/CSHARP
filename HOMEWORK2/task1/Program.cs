c8bd7307e2ed09aa9dfe0c804040f34e2884d1a4
Nikita <nix15@bk.ru>
2023-06-18 11:47:26 +0300

    HW2Task1isOK

--- HOMEWORK2/task1/Program.cs
+++ HOMEWORK2/task1/Program.cs
@@ -1,28 +1,17 @@
 ﻿Console.Write("Input Your Number: ");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 int secdig(int n)
 {
-while(true)
-{
-if(Math.Abs(n)<100 || Math.Abs(n)>999)
-{
-    Console.WriteLine("The Number is not Three-Digit! Type Another Number!");
-    n = int.Parse(Console.ReadLine());
+    while (true)
+    {
+        if (Math.Abs(n) < 100 || Math.Abs(n) > 999)
+        {
+            Console.WriteLine("The Number is not Three-Digit! Type Another Number!");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        else break;
+    }
+    return n;
 }
-else
-break;
-}
-    return Math.Abs(n/10%10);
-}
-Console.WriteLine($"The Second Digit of Your Number  is {secdig(num)}");
-
-//while(true)
-//{
-//    num = int.Parse(Console.ReadLine());
-//    if(Math.Abs(num)>99 && Math.Abs(num)<1000)
-//    {
-//        break;
-//    }
-//    Console.WriteLine("The Number is not Three-Digit! Type Another Number!");
-//}
-//Console.WriteLine($"The Second Digit of Your Number {num} is {Math.Abs(num/10%10)}");
+num = secdig(num);
+Console.WriteLine($"The Second Digit of Your Number {num} is {Math.Abs(num / 10 % 10)}");
