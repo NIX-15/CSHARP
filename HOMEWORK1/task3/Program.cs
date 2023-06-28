8c1e685456804bb2d72e10f1371cf0a3198bad99
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-28 18:28:52 +0300

    hw2t3up

--- HOMEWORK1/task3/Program.cs
+++ HOMEWORK1/task3/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Input Your Number: ");
-int num = int.Parse(Console.ReadLine());//Да, почему-то всегда выдаются предупреждения при методе parse.
+int num = int.Parse(Console.ReadLine()!);//Да, почему-то всегда выдаются предупреждения при методе parse.
 if (num % 2 != 0)
     Console.WriteLine($"Your Number {num} is Odd!");
 else
