35f5c4121c969a8df2a62b96888ebf00c3d9b0e7
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-30 19:37:58 +0300

    sem3t2

---
+++ SEMINAR3/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿bool numcheck(int num)
+{
+    if(num<1 || num>4)
+    {
+        Console.WriteLine("Invalid Input!");
+        return false;
+    }
+    else return true;
+}
+void quartnum(int num)
+{
+    switch (num)
+    {
+        case 1:
+        Console.WriteLine("X_POS+, Y_POS+");
+        break;
+        case 2:
+        Console.WriteLine("X_NEG-, Y_POS+");
+        break;
+        case 3:
+        Console.WriteLine("X_NEG-, Y_NEG-");
+        break;
+        default:
+        Console.WriteLine("X_POS+, Y_NEG-");
+        break;
+    }
+}
+Console.Write("Enter the number of Quarter: ");
+int number = int.Parse(Console.ReadLine()!);
+if(numcheck(number)) quartnum(number);
