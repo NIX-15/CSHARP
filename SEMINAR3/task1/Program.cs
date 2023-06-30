cc36541c232ba8c5f09a53ad66d4758b404e73d3
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-06-30 18:58:28 +0300

    sem3t1up1

--- SEMINAR3/task1/Program.cs
+++ SEMINAR3/task1/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Enter the point coordinates (X and Y):");
-string pointquater (double x, double y)
+string pointquarter (double x, double y)
 {
-    if(x>0 && y>0) return $"Point ({x},{y}) locates in 1st Quater!";
-    if(x>0 && y<0) return $"Point ({x},{y}) locates in 4rd Quater!";
-    if(x<0 && y>0) return $"Point ({x},{y}) locates in 2nd Quater!";
-    if(x<0 && y<0) return $"Point ({x},{y}) locates in 3rd Quater!";
+    if(x>0 && y>0) return $"Point ({x},{y}) locates in 1st Quarter!";
+    if(x>0 && y<0) return $"Point ({x},{y}) locates in 4rd Quarter!";
+    if(x<0 && y>0) return $"Point ({x},{y}) locates in 2nd Quarter!";
+    if(x<0 && y<0) return $"Point ({x},{y}) locates in 3rd Quarter!";
     else return "Error! The coordinates must not be zero!";
 }
 /*bool pointzero (int x, int y)
@@ -18,5 +18,5 @@ string pointquater (double x, double y)
 }*/
 double X = double.Parse(Console.ReadLine()!);
 double Y = double.Parse(Console.ReadLine()!);
-string res = pointquater(X,Y); 
+string res = pointquarter(X,Y); 
 Console.WriteLine(res);
