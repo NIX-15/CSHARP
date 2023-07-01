878a929ba09909a1f5f01f20c7e976e4a0353573
Nikita <nix15@bk.ru>
2023-07-01 14:38:28 +0300

    sem3t4up2

--- SEMINAR3/task4/Program.cs
+++ SEMINAR3/task4/Program.cs
@@ -1,4 +1,4 @@
-﻿void squadrow()
+﻿void digrow()
 {
     int num = int.Parse(Console.ReadLine()!);
     Console.Write("Enter the digree: ");
@@ -8,23 +8,13 @@
     {
         int i = num / Math.Abs(num);
         int k = 1;
-        while (k != Math.Abs(num + 1))
+        while (k != Math.Abs(num) + 1)
         {
             Console.Write(Math.Pow(i, dig) + " ");
             k++;
             i = num / Math.Abs(num) * k;
         }
     }
-    /*else if (num < 0)
-    {
-        for (int i = -1; i >= num; i--)
-            Console.Write(Math.Pow(i, dig) + " ");
-    }
-    else
-    {
-        for (int i = 1; i <= num; i++)
-            Console.Write(Math.Pow(i, dig) + " ");
-    }*/
 }
 Console.Write("Enter the number: ");
-squadrow();//in progress
+digrow();
