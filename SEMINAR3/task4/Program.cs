94b9a85bd2743bd5404ff64a9c39e911fd10e1a4
Nikita <nix15@bk.ru>
2023-07-01 10:37:13 +0300

    sem3t4

---
+++ SEMINAR3/task4/Program.cs
@@ -0,0 +1,8 @@
+﻿void squadrow()
+{
+    int num = int.Parse(Console.ReadLine()!);
+    for (int i = 1; i <= Math.Abs(num); i++)
+        Console.Write(Math.Pow(i, 2) + " ");
+}
+Console.Write("Enter the number: ");
+squadrow();
