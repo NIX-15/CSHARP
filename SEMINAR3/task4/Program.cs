8437483e29633a5e8bbcd6a7c0cb26dce589f0a2
Nikita <nix15@bk.ru>
2023-07-01 11:15:19 +0300

    sem3t4up1

--- SEMINAR3/task4/Program.cs
+++ SEMINAR3/task4/Program.cs
@@ -1,8 +1,12 @@
 ﻿void squadrow()
 {
     int num = int.Parse(Console.ReadLine()!);
-    for (int i = 1; i <= Math.Abs(num); i++)
-        Console.Write(Math.Pow(i, 2) + " ");
+    if (num == 0) Console.WriteLine("0 is 0 in any digree!");
+    else
+    {
+        for (int i = 1; i <= Math.Abs(num); i++)
+            Console.Write(Math.Pow(i, 2) + " ");
+    }
 }
 Console.Write("Enter the number: ");
 squadrow();
