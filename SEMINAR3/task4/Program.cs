1562012d6eb1cacf172917b1a1d4cb9f37250bde
Nikita <nix15@bk.ru>
2023-07-01 12:10:05 +0300

    sem3t4inprogress

--- SEMINAR3/task4/Program.cs
+++ SEMINAR3/task4/Program.cs
@@ -1,12 +1,30 @@
 ﻿void squadrow()
 {
     int num = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter the digree: ");
+    int dig = int.Parse(Console.ReadLine()!);//пока нет проверки на ввод целочисленного значения степени
     if (num == 0) Console.WriteLine("0 is 0 in any digree!");
     else
     {
-        for (int i = 1; i <= Math.Abs(num); i++)
-            Console.Write(Math.Pow(i, 2) + " ");
+        int i = num / Math.Abs(num);
+        int k = 1;
+        while (k != Math.Abs(num + 1))
+        {
+            Console.Write(Math.Pow(i, dig) + " ");
+            k++;
+            i = num / Math.Abs(num) * k;
+        }
     }
+    /*else if (num < 0)
+    {
+        for (int i = -1; i >= num; i--)
+            Console.Write(Math.Pow(i, dig) + " ");
+    }
+    else
+    {
+        for (int i = 1; i <= num; i++)
+            Console.Write(Math.Pow(i, dig) + " ");
+    }*/
 }
 Console.Write("Enter the number: ");
-squadrow();
+squadrow();//in progress
