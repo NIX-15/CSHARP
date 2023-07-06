bb73c3de9b8bb7e8f6f53a4efe92d23a8f1eb0aa
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-06 19:14:59 +0300

    l3t1

---
+++ LESSON3/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Multiplying table
+void multi()
+{
+    for (int i = 1; i<11; i++)
+    {
+        for(int j = 1; j<11; j++)
+        Console.Write($"{i}x{j}={i*j} ");
+        Console.WriteLine();
+    }
+}
+multi();
