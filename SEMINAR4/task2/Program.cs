fb6e981c4b44bde20bef4cdd0645136105c4b308
Nikita <nix15@bk.ru>
2023-07-08 14:10:33 +0300

    sem4t2

---
+++ SEMINAR4/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the number:");
+/*string number = Console.ReadLine()!;
+int dignum(string d)
+{
+    return d.Length;
+}
+Console.WriteLine($"{number}->{dignum(number)}");*/
+int[] array = new int[new Random().Next(10, 20)];
+int dignum(int[] arr)
+{
+    return arr.Length;
+}
+Console.WriteLine($"Number of digits is {dignum(array)}");
