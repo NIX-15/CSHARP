680e926aa6d79d9f7f42eddc06fa851d044ccc95
Nikita <nix15@bk.ru>
2023-07-08 13:18:03 +0300

    sem4t1

---
+++ SEMINAR4/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine()!);
+int summary(int s)
+{
+
+    int sum = 0;
+    for (int i = 0; i < num + 1; i++)
+        sum += i;
+    return sum;
+}
+Console.WriteLine($"Summary:{summary(num)}");
