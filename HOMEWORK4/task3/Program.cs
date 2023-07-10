e54e838b94376dc36545c3947c69dd089a1b8252
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-10 14:57:37 +0300

    hw4t3

---
+++ HOMEWORK4/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the dimension of array: ");
+int dimension = int.Parse(Console.ReadLine()!);
+void fillarray(int dim)
+{
+    if (dim < 1)
+        Console.WriteLine("Error! Dimension of Array must be greater than 0!");
+    else
+    {
+        double[] array = new double[dim];
+        for (int i = 0; i < array.Length; i++)
+            array[i] = new Random().Next(-99, 100);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+    fillarray(dimension);
