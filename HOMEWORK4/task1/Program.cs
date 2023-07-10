e8190135e17b15eb9593e12fd463c3589ddf6891
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-10 14:32:53 +0300

    hw4t2

---
+++ HOMEWORK4/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the number: ");
+double indigree (double num, double dig)
+{
+    return Math.Pow(num,dig);
+}
+double number = double.Parse(Console.ReadLine()!);
+Console.Write("Enter the digree: ");
+double digree = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number}^{digree} = {indigree(number,digree)}");
---
+++ HOMEWORK4/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine()!);
+int numsum(int num)
+{
+    int result = 0;
+    for(;;)
+    {
+        if(Math.Abs(num)>9)
+        {
+            result+=num%10;
+            num/=10;
+        }
+        else break;
+    }
+    result+=num;
+    return result;
+}
+Console.WriteLine($"The sum of the digits of {number}->{Math.Abs(numsum(number))}");
