156866b27a8e0f21f55c0db00c262ab846b619cf
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-13 13:48:57 +0300

    hw5t3

---
+++ HOMEWORK5/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿int dimension = new Random().Next(1, 21);
+double[] array = new double[dimension];
+void arrfill()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble()*(100-0)+0;
+        Console.Write($"{Math.Round(array[i], 2)} ");
+    }
+    Console.WriteLine();
+}
+void minmax()
+{
+    double min=array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>max)
+        max=array[i];
+        else if(array[i]<min)
+        min = array[i];
+    }
+    Console.WriteLine($"Max.Element = {Math.Round(max, 2)}");
+    Console.WriteLine($"Min.Element = {Math.Round(min, 2)}");
+    Console.WriteLine($"{Math.Round(max, 2)}-{Math.Round(min, 2)} = {Math.Round(max, 2)-Math.Round(min, 2)}");
+}
+arrfill();
+minmax();
