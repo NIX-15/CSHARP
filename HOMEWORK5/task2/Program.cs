defd403366dd0774027c5db6af2a8e3d726d1166
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-13 13:00:04 +0300

    hw5t2

---
+++ HOMEWORK5/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿int dimension = new Random().Next(1, 21);
+int[] array = new int[dimension];
+void arrfill()
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-99, 100);
+}
+int oddsum()
+{
+    int oddres = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)//отсчёт от 0 поэтому пишем как будто для чётных. Иначе можно (if((i%2)+1)!=0)
+            oddres += array[i];
+    }
+    return oddres;
+}
+void displayArr()
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+arrfill();
+displayArr();
+Console.WriteLine($"Sum of odd elements is {oddsum()}");
