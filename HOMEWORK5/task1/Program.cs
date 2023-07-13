8228f25a1941cb26265373c5496443b9e5f62456
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-13 12:28:16 +0300

    hw5t1final

--- HOMEWORK5/task1/Program.cs
+++ HOMEWORK5/task1/Program.cs
@@ -1,26 +1,35 @@
 ﻿Console.Write("Enter the dimension of array: ");
 int dimension = int.Parse(Console.ReadLine()!);
-void fillarray(int dim)
+double[] array = new double[dimension];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+bool dimcheck(int dim)
 {
-    int evencount=0;
     if (dim < 1)
-        Console.WriteLine("Error! Dimension of Array must be greater than 0!");
-    else
     {
-        double[] array = new double[dim];
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = new Random().Next(100, 1000);
-            if(array[i]%2==0)
-            evencount++;
-        }
-
-        for (int i = 0; i < array.Length; i++)
-        {
-            Console.Write($"{array[i]} ");
-        }
-        Console.WriteLine();
-        Console.WriteLine($"Here is {evencount} even number(s)!");
+        Console.WriteLine("Error! Dimension of Array must be greater than 0!");
+        return false;
     }
+    else return true;
 }
-    fillarray(dimension);//in progress
+int arrayeven()
+{
+    int evencount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            evencount++;
+    }
+    return evencount;
+}
+void displayArr()
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+if(dimcheck(dimension))
+{
+    displayArr();
+    Console.WriteLine($"Here is {arrayeven()} even number(s)!");
+}
