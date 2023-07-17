956ef267a9066553c18eb0f7af3f29304695aced
Nikita <nix15@bk.ru>
2023-07-17 20:09:17 +0300

    l4t1

---
+++ LESSON4/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿void matinit(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+void displaymat(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+matinit(matrix);
+displaymat(matrix);
