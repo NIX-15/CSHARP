1d1bc810d8b07d8fb8d2122bd4bd7d28db7143cb
Nikita <nix15@bk.ru>
2023-07-19 22:24:56 +0300

    s6t2

--- SEMINAR6/task2/Program.cs
+++ SEMINAR6/task2/Program.cs
@@ -1,8 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Enter the number: ");
-int number = int.Parse(Console.Readline()!);
-string binar = string.Empty;
-while(number>1)
+int number = int.Parse(Console.ReadLine()!);
+
+
+void rev(string s)
 {
-    //in progress
+    for (int i = s.Length - 1; i >= 0; i--)
+        Console.Write(s[i]);
+    Console.WriteLine();
 }
+string binary(int num)
+{
+    string binar = string.Empty;
+    while (num > 1)
+    {
+        binar += Convert.ToString(num % 2);
+        num /= 2;
+    }
+    binar += Convert.ToString(num);
+    return binar;
+}
+rev(binary(number));
