6ea66f2fedb99f70a7fe3fd30283ac41d999659d
Nikita <nix15@bk.ru>
2023-07-19 23:35:31 +0300

    hw6t1

---
+++ HOMEWORK6/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter the numbers:");
+int poscount()
+{
+    int count = 0;
+    while (true)
+    {
+        int num = int.Parse(Console.ReadLine()!);
+        //Console.WriteLine(num);
+        if (num > 0)
+            count++;
+        else if (num == 0)
+            break;
+    }
+    return count;
+}
+Console.WriteLine($"Positive numbers = {poscount()}");
---
+++ SEMINAR6/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter the number:");
+int number = int.Parse(Console.ReadLine()!);
+void fibonacci(int num)
+{
+    string fib = "01";
+    int p0 = 0; int p1 = 1;
+    for (int i = 2; i < num; i++)
+    {
+        int sum = p0 + p1;
+        fib += Convert.ToString(sum);
+        p0 = p1; p1 = sum;
+    }
+    Console.WriteLine(fib);
+}
+if (number > 0)
+    fibonacci(number);
