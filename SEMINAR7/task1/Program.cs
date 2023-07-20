d37cefa05e36ca132094342c8e0511970e9af488
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-20 20:41:40 +0300

    s7t1

---
+++ SEMINAR7/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void fillMatrix()
+{
+    double[,] mat = new double[new Random().Next(1, 11), new Random().Next(1, 11)];
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            if ((i + 1) % 2 == 0 && (j + 1) % 2 == 0)
+            {
+                mat[i, j] = Math.Pow(new Random().NextDouble() * (10 + 9) - 9, 2);
+                Console.Write($"!{Math.Round(mat[i, j], 2)}!  ");
+            }
+            else
+            {
+                mat[i, j] = new Random().Next(-9, 10);
+                Console.Write($"{Math.Round(mat[i, j], 2)}  ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+fillMatrix();
