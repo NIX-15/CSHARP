9ae917f35ad0af4e257f6ae431f667efad6c337c
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-20 20:46:43 +0300

    s7t2

---
+++ SEMINAR7/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void fillMatrix()
+{
+    int[,] mat = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat[i, j] = new Random().Next(-9, 10);
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+fillMatrix();//in progress
