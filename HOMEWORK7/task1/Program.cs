7fe43ac56c279db8123ab90c313619cf44f4eb1f
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-20 14:35:39 +0300

    hw7t1

---
+++ HOMEWORK7/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+void displaymat(double[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(mat[i, j],2)}  ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+
+double[,] matinit()
+{
+    double[,] mat = new double[new Random().Next(1, 11), new Random().Next(1, 11)];
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            mat[i, j] = new Random().NextDouble()*(100+99)-99;
+        }
+    }
+    return mat;
+}
+displaymat(matinit());
