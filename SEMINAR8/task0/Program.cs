ad8f85475881e466ce75ec9f6e131f2e1d3bd0fe
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-21 17:41:35 +0300

    s8t0..

---
+++ SEMINAR8/task0/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] matinit()
+{
+    int[,] matrix = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return matrix;
+}
+/*void displayMat(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}*/
+void rowswap(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    for (int i = 0; i < mat.GetLength(1); i++)
+        (mat[0, i], mat[mat.GetLength(0), i]) = (mat[mat.GetLength(0), i], mat[0, i]);
+
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//displayMat(matinit());
+rowswap(matinit());//in progress
