e8b584f56c8e81e90dfc5dcc6e673d48b2857e21
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-21 17:05:13 +0300

    s7t2

--- SEMINAR7/task2/Program.cs
+++ SEMINAR7/task2/Program.cs
@@ -1,6 +1,7 @@
-﻿void fillMatrix()
+﻿int[,] fillMatrix()
 {
-    int[,] mat = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+    int dimension = new Random().Next(1, 11);
+    int[,] mat = new int[dimension, dimension];
     for (int i = 0; i < mat.GetLength(0); i++)
     {
         for (int j = 0; j < mat.GetLength(1); j++)
@@ -11,5 +12,13 @@
         Console.WriteLine();
     }
     Console.WriteLine();
+    return mat;
 }
-fillMatrix();//in progress
+int diagsum(int[,] mat)
+{
+    int result = 0;
+    for (int i = 0; i < mat.GetLength(0); i++)
+        result += mat[i, i];
+    return result;
+}
+Console.WriteLine($"Total Sum = {diagsum(fillMatrix())}");
