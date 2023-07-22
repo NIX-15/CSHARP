ccaf978fb62e13517de47a4c00ae33a276b35c02
Nikita Sechenov <nikitasechenov@192.168.1.7>
2023-07-22 15:44:40 +0300

    s8t1

---
+++ SEMINAR8/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,] matrix = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+void matfill()
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+int[,] transponate(int[,] mat)
+{
+    int[,] transe = new int[mat.GetLength(1), mat.GetLength(0)];
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            transe[j, i] = mat[i, j];
+        }
+    }
+    return transe;
+}
+void displayMat(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+matfill();
+displayMat(matrix);
+Console.WriteLine();
+displayMat(transponate(matrix));
