bb5d97e796de44dcd642dd5126a4b1709889d888
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-24 14:20:22 +0300

    s8t2

--- SEMINAR8/task2/Program.cs
+++ SEMINAR8/task2/Program.cs
@@ -1,4 +1,4 @@
-﻿int[,] matrix = new int[new Random().Next(1, 11), new Random().Next(1, 11)];
+﻿int[,] matrix = new int[new Random().Next(1, 8), new Random().Next(1, 8)];
 void matfill()
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -12,32 +12,28 @@ int[] datalist(int[,] mat)
     Console.WriteLine();
     int[] arrData = new int[1];
     arrData[0] = mat[0, 0];
-    //Array.Resize(ref arrData, arrData.Length + 1);
-    //arrData[arrData.Length - 1] = mat[0, 1];
     for (int i = 0; i < mat.GetLength(0); i++)
     {
-        for (int j = 1; j < mat.GetLength(1); j++)
+        for (int j = 0; j < mat.GetLength(1); j++)
         {
-            for (int t = 0; t < mat.GetLength(0); t++)
-            {
-                for (int k = 0; k < mat.GetLength(1); k++)
-                {
-                    if (mat[i, j] == mat[t, k] && i != t && j != k)
-                    Console.WriteLine($"Number {mat[t,k]} has already added to the array!");
-                    break;
-                }
-            }
-        
-            /*for (int t = 0; t < arrData.Length; t++)
+            int t = 0;
+            while (true)
             {
                 if (mat[i, j] == arrData[t])
                 {
-                    Console.WriteLine("Number has already added to the array!");
+                    if (t == 0)
+                        break;
+                //Console.WriteLine($"Number {mat[i, j]} has already added to the array!");
                     break;
                 }
-            }*/
-            Array.Resize(ref arrData, arrData.Length + 1);
-            arrData[arrData.Length - 1] = mat[i, j];
+                if (mat[i, j] != arrData[t] && t == arrData.Length - 1)
+                {
+                    Array.Resize(ref arrData, arrData.Length + 1);
+                    arrData[arrData.Length - 1] = mat[i, j];
+                    break;
+                }
+                t++;
+            }
         }
     }
     return arrData;
@@ -53,12 +49,25 @@ void displayMat(int[,] mat)
         Console.WriteLine();
     }
 }
-void displayArr(int[] arr)
+int[,] resultTable(int[,] mat, int[] arr)
 {
-    for (int j = 0; j < arr.Length; j++)
-        Console.Write($"{arr[j]} ");
+    int[,] result = new int[arr.Length, 2];
+    for (int i = 0; i < arr.Length; i++)
+        result[i, 0] = arr[i];
     Console.WriteLine();
+    for (int t = 0; t < result.GetLength(0); t++)
+    {
+        for (int i = 0; i < mat.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                if (mat[i, j] == result[t, 0])
+                    result[t, 1]++;
+            }
+        }
+    }
+    return result;
 }
 matfill();
 displayMat(matrix);
-displayArr(datalist(matrix));//in progress
+displayMat(resultTable(matrix, datalist(matrix)));
