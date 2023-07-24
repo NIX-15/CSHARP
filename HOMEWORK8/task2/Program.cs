ade7dae3efababd3191d1165e354a8d62145e741
Nikita <nix15@bk.ru>
2023-07-24 19:54:30 +0300

    hw8t2

---
+++ HOMEWORK8/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,] matrix = new int[new Random().Next(2, 7), new Random().Next(2, 7)];
+void matfill(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+            mat[i, j] = new Random().Next(-99, 100);
+    }
+}
+int[] sumCollect(int[,] mat)
+{
+    int[] sumTable = new int[mat.GetLength(0)];
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+            sumTable[i] += mat[i, j];
+    }
+    return sumTable;
+}
+void showLowestSum(int[,] mat, int[] sumtable)
+{
+    //
+}
+void displayMat(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
