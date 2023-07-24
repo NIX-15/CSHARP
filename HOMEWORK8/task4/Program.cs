4c69c98d79634c216608a4bda49eb06eb20432a1
Nikita Sechenov <nikitasechenov@192.168.1.4>
2023-07-24 22:25:06 +0300

    hw8t4

--- HOMEWORK8/task4/Program.cs
+++ HOMEWORK8/task4/Program.cs
@@ -9,7 +9,7 @@ void matfill(int[,] mat)
 }
 int[,] matrixReduction(int[,] mat)
 {
-    int[,] matred = new int[mat.GetLength(0)-1,mat.GetLength(1)-1];
+    int[,] matred = new int[mat.GetLength(0) - 1, mat.GetLength(1) - 1];
     int imin = 0;
     int jmin = 0;
     for (int i = 0; i < mat.GetLength(0); i++)
@@ -23,14 +23,14 @@ int[,] matrixReduction(int[,] mat)
             }
         }
     }
-    Console.WriteLine($"Min.Element = {mat[imin, jmin]}, row = {imin+1}, column = {jmin+1}");
+    Console.WriteLine($"Min.Element = {mat[imin, jmin]}, row = {imin + 1}, column = {jmin + 1}");
     Console.WriteLine();
     for (int i = 0; i < matred.GetLength(0); i++)
     {
         for (int j = 0; j < matred.GetLength(1); j++)
         {
-            if(i!=imin || j!=jmin)
-        matred[i,j] = mat[i,j];
+            if (i != imin && j != jmin)
+                matred[i, j] = mat[i, j];
         }
     }
     return matred;
