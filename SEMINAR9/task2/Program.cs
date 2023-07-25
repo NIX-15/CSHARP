d12258876f3504e634d1b485c56cac46a4040e06
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-25 20:12:21 +0300

    s9t2

---
+++ SEMINAR9/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 2: Задайте значения M и N. Напишите программу, 
+которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+ M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+ M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+int recurNum(int n1, int n2)
+{
+    if (n1 == n2)
+    {
+        return n1;
+    }
+    else
+    {
+        Console.Write($"{n1}, ");
+        return recurNum(n1 + 1, n2);
+    }
+}
+Console.Write("M-N: ");
+Console.WriteLine(recurNum(-6, 1));
