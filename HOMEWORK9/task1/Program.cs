c2ef7e5769bc54de4764f4817b73102d7995b15a
Nikita <nix15@bk.ru>
2023-07-26 22:25:51 +0300

    hw9t1

---
+++ HOMEWORK9/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"*/
+
+int numberM = -5;
+int numberN = 11;
+if (numberM > numberN)
+    Console.WriteLine("Invalid input! N must not be lower than M!");
+else
+{
+    Console.Write($"Even numbers between {numberM} and {numberN}: ");
+    Console.Write(recurEven(numberM, numberN));
+}
+int recurEven(int m, int n)
+{
+    if (m == n - 1 && n % 2 != 0 || m == n && n % 2 == 0)
+        return m;
+    else
+    {
+        if (m % 2 == 0)
+            Console.Write($"{m} ");
+        return recurEven(m + 1, n);
+    }
+}
--- SEMINAR9/task4/Program.cs
+++ SEMINAR9/task4/Program.cs
@@ -2,13 +2,11 @@
 возводит число А в целую степень B с помощью рекурсии.
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8*/
-int num = 0;
-int digree = 0;
+int num = -3;
+int digree = 3;
 
 int recurDig(int n, int d)
 {
-    if (d == 2)
-        return n * n;
     if (d == 1)
         return n;
     if (d == 0)
