168a50e9c01dd239e5677d65f8d709ab844612d4
Nikita <nix15@bk.ru>
2023-07-26 20:26:36 +0300

    s9t3

--- SEMINAR9/task3/Program.cs
+++ SEMINAR9/task3/Program.cs
@@ -1,11 +1,14 @@
-﻿/*Задача 3: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 
+﻿/*Задача 3: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 Использовать рекурсию.
 453 -> 12 45 -> 9*/
 
-int recurSum(int n)//in progress
-{
-    //if (n / 10 < 1)
-       // return n;
+int num = 5;
 
-    return recurSum(n/10);
+int recurSum(int n) //in progress
+{
+    if (n < 10)
+        return n;
+    n = n % 10;
+    return n + recurSum(n / 10);
 }
+Console.WriteLine($"Digits sum of {num} is {recurSum(num)}");
