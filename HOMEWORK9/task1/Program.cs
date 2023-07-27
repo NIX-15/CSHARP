194296cf007d78e3670ea3901754f7b6124f42fa
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-27 15:59:54 +0300

    hw9

--- HOMEWORK9/task1/Program.cs
+++ HOMEWORK9/task1/Program.cs
@@ -3,23 +3,30 @@
 M = 1; N = 5 -> "2, 4"
 M = 4; N = 8 -> "4, 6, 8"*/
 
-int numberM = -5;
-int numberN = 11;
-if (numberM > numberN)
-    Console.WriteLine("Invalid input! N must not be lower than M!");
-else
+int numberM = -19;
+int numberN = 32;
+bool numCheck(int m, int n)
 {
-    Console.Write($"Even numbers between {numberM} and {numberN}: ");
-    Console.Write(recurEven(numberM, numberN));
+    if (m > n)
+    {
+        Console.WriteLine("Invalid input! N must not be lower than M!");
+        return false;
+    }
+    return true;
 }
-int recurEven(int m, int n)
+void recurEven(int m, int n)
 {
     if (m == n - 1 && n % 2 != 0 || m == n && n % 2 == 0)
-        return m;
+        Console.WriteLine($"{m}");
     else
     {
         if (m % 2 == 0)
-            Console.Write($"{m} ");
-        return recurEven(m + 1, n);
+            Console.Write($"{m}, ");
+        recurEven(m + 1, n);
     }
+}
+if (numCheck(numberM, numberN))
+{
+    Console.Write($"Even numbers between {numberM} and {numberN}: ");
+    recurEven(numberM, numberN);
 }
