ed89b737a2c8b9f0d182a13241b195ef351cb03b
Nikita Sechenov <nikitasechenov@MacBook-Pro-Nikita.local>
2023-07-27 20:22:55 +0300

    s9t4up

--- SEMINAR9/task4/Program.cs
+++ SEMINAR9/task4/Program.cs
@@ -2,15 +2,17 @@
 возводит число А в целую степень B с помощью рекурсии.
 A = 3; B = 5 -> 243 (3⁵)
 A = 2; B = 3 -> 8*/
-int num = -3;
-int digree = 3;
+double num = 3;
+double digree = 3;
 
-int recurDig(int n, int d)
+double recurDig(double n, double d)
 {
-    if (d == 1)
+    if (d == 1 || n == 0 && d != 0)
         return n;
     if (d == 0)
         return 1;
+    if (d < 0)
+        return (1 / n) * recurDig(n, d + 1);
     return n * recurDig(n, d - 1);
 }
 Console.WriteLine($"{num}^{digree}={recurDig(num, digree)}");
